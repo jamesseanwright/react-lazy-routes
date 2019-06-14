1e859a5865e162377b2ecde46c927faae793bb0e
James Wright <james.wright@yld.io>
2019-06-14 16:01:13 +0100

    Adds Terser

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import nodeGlobals from 'rollup-plugin-node-globals';
 import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 export default ({ inlineDynamicImports }) => ({
   input: 'src/index.jsx',
@@ -19,6 +20,11 @@ export default ({ inlineDynamicImports }) => ({
     babel({
       exclude: 'node_modules/**',
       plugins: ['@babel/syntax-dynamic-import', '@babel/transform-react-jsx'],
+    }),
+    terser({
+      mangle: {
+        toplevel: true,
+      },
     }),
   ],
 });
